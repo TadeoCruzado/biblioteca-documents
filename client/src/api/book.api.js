00a10acefd9ff143569dd513bb0afe2f9277750a
carloscd <carlos17cruzado@gmail.com>
2024-01-20 20:06:55 -0500

    actualizacion con link actualizado

--- client/src/api/book.api.js
+++ client/src/api/book.api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 export const obtenerDatos = async() =>
-    await axios.get('http://localhost:3000/book');
+    await axios.get('https://biblioteca-full.onrender.com/book');
 
 export const createBook = async(book) => 
-    await axios.post('http://localhost:3000/book', book);
+    await axios.post('https://biblioteca-full.onrender.com/book', book);
 
 export const unSoloDatos = async (id) =>
-    await axios.get(`http://localhost:3000/book/${id}`);
+    await axios.get(`https://biblioteca-full.onrender.com/book/${id}`);
 
 export const eliminarDato = async(id) =>
-    await axios.delete(`http://localhost:3000/book/${id}`);
+    await axios.delete(`https://biblioteca-full.onrender.com/book/${id}`);
 
 export const editarDato = async (id, libroEdit) =>
-    await axios.put(`http://localhost:3000/book/${id}`, libroEdit);
+    await axios.put(`https://biblioteca-full.onrender.com/${id}`, libroEdit);
