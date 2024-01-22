0ee943a7b4aeefd4aad17053d84ecb2b8a0635b8
carloscd <carlos17cruzado@gmail.com>
2024-01-21 19:48:49 -0500

    Ya malogre todo

--- client/src/api/book.api.js
+++ client/src/api/book.api.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
+const link  = 'https://localhost:3000'
+
 export const obtenerDatos = async() =>
-    await axios.get('https://biblioteca-full.onrender.com/book');
+    await axios.get(`https://localhost:3000/book`);
 
 export const createBook = async(book) => 
-    await axios.post('https://biblioteca-full.onrender.com/book', book);
+    await axios.post(`https://localhost:3000/book`, book);
 
 export const unSoloDatos = async (id) =>
-    await axios.get(`https://biblioteca-full.onrender.com/book/${id}`);
+    await axios.get(`https://localhost:3000/book/${id}`);
 
 export const eliminarDato = async(id) =>
-    await axios.delete(`https://biblioteca-full.onrender.com/book/${id}`);
+    await axios.delete(`https://localhost:3000/book/${id}`);
 
 export const editarDato = async (id, libroEdit) =>
-    await axios.put(`https://biblioteca-full.onrender.com/${id}`, libroEdit);
+    await axios.put(`https://localhost:3000/book/${id}`, libroEdit);
--- client/src/pages/LibroInfo.jsx
+++ client/src/pages/LibroInfo.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Header } from '../components/Header'
 import { useBook } from '../context/LibroProvider'
 import { useNavigate, useParams } from 'react-router-dom';
-import '../components/styles/libroinfo.css'
+
 import {Footer} from '../components/Footer'
 
 const LibroInfo = () => {
